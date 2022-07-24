ede799906aac92382e8703f49988168b8dbcc6ec
Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>
2022-07-24 08:37:55 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(
+      body: Obx(                       //Never miss this shit
         () => Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
