d209a993bef4de5859f2d4aeb81de864e5c5c921
Dineth Siriwardana <91774218+dinethsiriwardana@users.noreply.github.com>
2022-08-28 07:59:30 +0530

    Delete counterCountrollers.dart

--- lib/controllers/counterCountrollers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-
-class CounterController extends GetxController {
-  var counter = 0.obs;
-  void increment() {
-    counter++;
-  }
-}
